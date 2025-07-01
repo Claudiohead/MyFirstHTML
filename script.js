1b0eaf443e52d31d23f1ad30fe21bb000cd8005d
Claudiohead <claudiohead@gmail.com>
2025-07-01 14:24:18 -0300

    let

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ function inserirNome(){
 }
 
 inserirNome();
+let linguagens = ["JavaScript","Python","C++"]
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1]) // “Python”
+console.log(linguagens[2]) // “C”
