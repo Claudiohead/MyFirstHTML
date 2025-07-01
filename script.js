646f758dea4669af09a3c8534577a531def15025
Claudiohead <claudiohead@gmail.com>
2025-07-01 14:40:54 -0300

    .

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ let linguagens = ["JavaScript","Python","C++"]
 console.log(linguagens[0]) // “JavaScript”
 console.log(linguagens[1]) // “Python”
 console.log(linguagens[2]) // “C”
+
+const item = document.querySelector("#lista");
+item.textContent = linguagens[0];
+item.textContent = linguagens[1];
+item.textContent = linguagens[2];
